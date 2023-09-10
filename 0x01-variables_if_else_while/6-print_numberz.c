2daaf20883e6ccb0fab85a733a3134b81b6fa666
mohammed <mohammedadilmubarek@gmail.com>
2023-09-10 02:02:09 -0700

    numz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	for(n = 49; n <= 57; n++)
+	{
+	       putchar(n);
+	}
+        putchar('\n');
+       
+        return (0);
+}	
