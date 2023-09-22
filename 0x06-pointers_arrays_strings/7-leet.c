49906b57b0c6b23cdb4c9b5148346960c27ee820
mohammed <mohammedadilmubarek@gmail.com>
2023-09-22 13:28:37 -0700

    c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,7 +15,7 @@ char *leet(char *c)
 
 	while (*c)
 	{
-		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		for (i = 0; i < sizeof(key); i++)
 		{
 			if (*c == key[i] || *c == key[i] + 32)
 			{
