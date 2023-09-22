d01ca5d7c346a130eacc859c99a2aaaae94b9088
mohammed <mohammedadilmubarek@gmail.com>
2023-09-22 05:54:05 -0700

    c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if the tow strings are same, other int otherwise
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int eq;
+
+	while (*s1)
+	{
+		if (*s1 != *s2)
+			eq += ((int)*s1 - (int)*s2);
+		s1++;
+		s2++;
+	}
+
+	return (eq);
+}
