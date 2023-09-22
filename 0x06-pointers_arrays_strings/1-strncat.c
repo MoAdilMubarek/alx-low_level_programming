1cebb597862d9e55b32f4ae45c9aaa55a3a8d7b2
mohammed <mohammedadilmubarek@gmail.com>
2023-09-22 05:23:05 -0700

    c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,4 +2,26 @@
 
 /**
  * *_strncat - concatenates two strings
- * @dest: pointer to destination */
+ * @dest: pointer to destination
+ * @src: pointer to source
+ * @n: int parameter
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i])
+		i++;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[i++] = src[j];
+
+	dest[i + j] = '\0';
+
+	return (dest);
+}
+
