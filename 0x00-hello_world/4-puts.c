bef80e53c6598b433376da987fdd01a4783de0b4
mohammed <mohammedadilmubarek@gmail.com>
2023-10-04 23:51:41 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * Return: 0
  */
 
-int main()
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
