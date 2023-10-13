8add72497958989de449dfdea79f52207432d683
MoAdilMubarek <140736985+MoAdilMubarek@users.noreply.github.com>
2023-10-13 09:53:37 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,7 +8,7 @@
         {
             for (j = hi; j <= size; j++)
             {
-                _putchar(' ');
+                _putchar('  ');
             }   
             for (wid = 1; wid <= hi; wid++)
             {
