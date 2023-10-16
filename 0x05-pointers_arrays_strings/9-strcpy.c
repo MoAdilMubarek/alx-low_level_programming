327a8dfce4a08ac67087cbda1ce32657d62690f2
mohammed <mohammedadilmubarek@gmail.com>
2023-10-16 12:46:17 -0700

    c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int j = 0;
 
-	while (src[j] <= '\0')
+	while (src[j] != '\0')
 	{
 		dest[j] = src[j];
 		j++;
 	}
+	dest[j] = '\0';
 
 	return (dest);
 }
