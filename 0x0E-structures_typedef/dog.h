e999556cfd42f4fced28b73bf7a82eed34690f79
mohammed <mohammedadilmubarek@gmail.com>
2023-11-07 10:59:56 -0800

    c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog: dog struct
+ * @name: pointer to char
+ * @age: float
+ * @owner: pointer to char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
