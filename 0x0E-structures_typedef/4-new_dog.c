66f88fc32dd8c7ddfb12ee033ca8bf3e831854de
mohammed <mohammedadilmubarek@gmail.com>
2023-11-07 13:34:11 -0800

    c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: pointer
+ * @age: float
+ * @owner: pointer
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t.name = name;
+
+	
