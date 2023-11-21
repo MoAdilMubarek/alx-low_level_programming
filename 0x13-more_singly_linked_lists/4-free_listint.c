f7269dd43c69e32ee5982e73083c5688bdc862fc
mohammed <mohammedadilmubarek@gmail.com>
2023-11-21 11:22:33 -0800

    c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer
+ */
+
+void free_listint(listint_t *head)
+{
+	while (head)
+	{
+		free(head);
+		head->next;
+	}
+}
+
+
