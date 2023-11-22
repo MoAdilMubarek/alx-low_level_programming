a28a4bad84b3d626d4cee6f03dbabf80a0508bad
mohammed <mohammedadilmubarek@gmail.com>
2023-11-22 12:49:21 -0800

    c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,13 @@
 
 void free_listint2(listint_t **head)
 {
+	listint_t *current;
+	listint_t *next;
 	if (!head || !*head)
 	{
 		return;
 	}
-	listint_t *current = *head;
-	listint_t *next;
+	current	= *head;
 
 	while (current)
 	{
