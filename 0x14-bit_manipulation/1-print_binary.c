eed7e68129778c34f0a97c43077be7d5d7a10c6c
mohammed <mohammedadilmubarek@gmail.com>
2023-11-24 12:20:11 -0800

    c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,6 +9,17 @@ void print_binary(unsigned long int n)
 {
 	int i, j;
 	unsigned long int var, k;
+
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 	       
 	for (j = sizeof(unsigned long int) * 8 - 1; j >= 0; j--)
 	{
@@ -16,6 +27,7 @@ void print_binary(unsigned long int n)
 		var = 1 << i;
 		k = n&var;
 		k == 1 ? _putchar('1') : _putchar('0');
-	}	
+	}
+}	
 
 
