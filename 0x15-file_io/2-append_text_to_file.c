b2fe4ff9224839f4a2543501cdea76b4a218cb83
mohammed <mohammedadilmubarek@gmail.com>
2023-11-28 12:15:36 -0800

    c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: pointer to a file
+ * @text_content: pointer
+ * Return: 1 or -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int des;
+	ssize_t size = 0, bytes;
+
+	if (filename == NULL)
+		return (-1);
+
+	des = open(filename, O_WRONLY);
+
+	if (des == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (*text_content++)
+			size++;
+
+		bytes = write(des, text_content, size);
+
+		return (size == bytes? 1 : -1);
+	}
+
+	close(des);
+
+}
+
+
