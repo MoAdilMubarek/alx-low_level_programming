b87c72f53d8823c5fa3257de8dac66488c370e6e
mohammed <mohammedadilmubarek@gmail.com>
2023-11-30 11:09:06 -0800

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 			size++;
 	}
 
-	des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR, S_IWUSR);
+	des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (des < 0)
 		return (-1);
 
