a44f84a3abe2272827816709fc84a23a58a30e83
MoAdilMubarek <mohammedadilmubarek@gmail.com>
2024-01-12 12:57:20 -0800

    py

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,11 +8,11 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *current;
+	const dlistint_t *current;
 	size_t i;
 
 	current = h;
-	i = 0
+	i = 0;
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
