d33fed070e4111baf9fa171b598ab98dd752669a
MoAdilMubarek <mohammedadilmubarek@gmail.com>
2024-01-14 11:07:47 -0800

    py

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,15 +12,23 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+	if (!new || !head)
+		return (new? free(new), NULL : NULL);
 
 	new->n = n;
-	new->next = *head;
 	new->prev = NULL;
-	(*head)->prev = new;
-	(*head)->next = NULL;
-	*head = new;
+
+	if (!*head)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		*head = new;
+	}
 
 	return (new);
 }
